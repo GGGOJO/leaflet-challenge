5d5682a179339278cf0ff88a564f224229eef706
Gigi Jones <72949687+GGGOJO@users.noreply.github.com>
2021-04-14 21:11:18 -0700

    Update logic.js

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -8,29 +8,11 @@ let graymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
     accessToken: API_KEY
 });
 
-let satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/satellite-v9",
-    accessToken: API_KEY
-});
-
-let outdoorsmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/outdoors-v11",
-    accessToken: API_KEY
-});
-
 // Create a map object to place graymap, satellitemap and outdoorsmap layers to display on load
 var myMap = L.map("map-id", {
     center: [39.8, -94.5],
     zoom: 3,
-    layers: [graymap, satellitemap, outdoorsmap]
+    layers: [graymap]
 });
 
 // Adding the 'graymap' as the tile layer to the map
@@ -42,9 +24,7 @@ let earthquakes = new L.LayerGroup();
 
 // Set up an object to hold all of the maps
 let baseMaps = {
-    Satellite: satellitemap,
-    Outdoors: outdoorsmap,
-    Grayscale: graymap,
+    Grayscale: graymap
 };
 
 // Set up an object to hold all overlays.
